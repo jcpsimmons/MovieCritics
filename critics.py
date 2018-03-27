3386fa3ce667502ce8cb6bab30f001e0ded597b1
Josh Simmons - macbook <jcpsimmons@gmail.com>
2018-03-27 14:48:58 -0700

    working

---
+++ critics.py
@@ -0,0 +1,26 @@
+import urllib2
+from bs4 import BeautifulSoup
+#repl. with a way to scrape this from https://www.rottentomatoes.com/critics/authors
+
+name_list = ["armond-white", "anthony-lane", "peter-rainer"]
+
+critics = {}
+
+## initialize critics key value pair with score
+for name in name_list:
+    critics[name] = .5
+
+#testing
+
+test_page = "https://www.rottentomatoes.com/critic/armond-white/movies"
+#gets the html
+page = urllib2.urlopen(test_page)
+#parse to BS4 format
+soup = BeautifulSoup(page, 'html.parser')
+selection = soup.select("a.movie-link")
+print selection[1].string
+print len(selection)
+
+## math - if critic agrees take difference between current score and score
+## in pos or negative direction depending on if pos or neg. divide
+## by two and add or subtract to the score
