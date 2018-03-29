a3b334fdd4f9bedd028e1e707a07a94fbc2e1af4
Josh Simmons - macbook <jcpsimmons@gmail.com>
2018-03-29 15:47:45 -0700

    working on jr jones
    so far it's working on jr jones page but the way im finding reviews is by hardcoding how far down in the html it is, I need a way first to be able to transpose this point up or down, and second to find an automated way to set this transpose number. also error handling would be some icing on the cake.

--- critics.py
+++ critics.py
@@ -1,33 +1,87 @@
 import urllib2
 from bs4 import BeautifulSoup
-#repl. with a way to scrape this from https://www.rottentomatoes.com/critics/authors
 
-name_list = ["armond-white", "anthony-lane", "peter-rainer"]
 
+# to be replaced with scraping from the critics list?
+name_list = ["armond-white", "anthony-lane", "peter-rainer", "j-r-jones"]
 critics = {}
-
 ## initialize critics key value pair with score
 for name in name_list:
-    critics[name] = .5
+    critics[name] = 0.
+
+def increase_autonomy(critic_name):
+    # get the current score
+    current_score = critics[critic_name]
+    new_score = (1 - current_score) * 0.5
+    critics[critic_name] = critics[critic_name] + new_score
+    return
+
+def decrease_autonomy(critic_name):
+    # get the current score
+    if critics[critic_name] > 0.:
+        current_score = critics[critic_name]
+        new_score = (1 - current_score) * 0.5
+        critics[critic_name] = critics[critic_name] - new_score
+    else:
+        return
+    return
+
 
 #testing
 
-test_page = "https://www.rottentomatoes.com/critic/armond-white/movies"
+test_page = "https://www.rottentomatoes.com/critic/j-r-jones/movies"
 #gets the html
 page = urllib2.urlopen(test_page)
 #parse to BS4 format
 soup = BeautifulSoup(page, 'html.parser')
-corpus = soup.select('#criticsReviewsChart_main')
-print corpus
+# Reviews start at line 53 end with 249
+corpus = soup.select('td')
+# lop off array outside of 53 - 249
+for i in range(0,53):
+    del(corpus[0])
+for i in range(0,(len(corpus) - 196)):
+    del(corpus[196])
 
-# for row in soup.findAll('table')[0].tbody.findAll('tr'):
-#     first_column = row.findAll('th')[0].contents
-#     third_column = row.findAll('td')[2].contents
-#     print first_column, third_column
 
-# print selection[1].string
-# print len(selection)
+# 1,2,5,6,9,10
+# two pair streams each counting by four, generate one, clone it and add 1 to all values, then interleave (zip) the two lists
+reviewer_rating_indices = range(1, len(corpus), 4)
+rottentomatoes_rating_indices = range(2, len(corpus), 4)
+# new_list = index_pattern_one + index_pattern_two
+# new_list.sort() # automatically stores into list.
+# actually keep them as separate
 
-## math - if critic agrees take difference between current score and score
-## in pos or negative direction depending on if pos or neg. divide
-## by two and add or subtract to the score
+critic_reviews = []
+rottentomatoes_reviews = []
+
+for i in reviewer_rating_indices:
+    critic_reviews.append(corpus[i])
+for i in rottentomatoes_rating_indices:
+    rottentomatoes_reviews.append(corpus[i])
+
+for i, value in enumerate(critic_reviews):
+    if str(value).find('fresh') != -1:
+        critic_reviews[i] = 1
+    else:
+        critic_reviews[i] = 0
+
+for i, value in enumerate(rottentomatoes_reviews):
+    if str(value).find('fresh') != -1:
+        rottentomatoes_reviews[i] = 1
+    else:
+        rottentomatoes_reviews[i] = 0
+
+for i, value in enumerate(critic_reviews):
+    if value == rottentomatoes_reviews[i]:
+        decrease_autonomy('j-r-jones')
+    else:
+        increase_autonomy('j-r-jones')
+
+print critics
+
+
+
+    # if str(value).find('fresh') != -1 and str(rottentomatoes_reviews[i]).find('fresh') != -1:
+    #     print "agree fresh!"
+    # if str(value).find('rotten') != -1 and str(rottentomatoes_reviews[i]).find('rotten') != -1:
+    #     print "agree rotten!"
---
+++ quickTests.py
@@ -0,0 +1,13 @@
+index_pattern_one = range(0, 100, 4)
+index_pattern_two = range(1, 100, 4)
+
+print index_pattern_one
+print index_pattern_two
+
+new = index_pattern_one + index_pattern_two
+
+print new
+
+new.sort()
+
+print new
