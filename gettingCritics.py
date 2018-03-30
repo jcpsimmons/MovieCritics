148b1bd98a5f6ab5c16672da84d8faa51a598daf
simsies <jcpsimmons@gmail.com>
2018-03-29 18:08:49 -0700

    got csv of critics

--- gettingCritics.py
+++ gettingCritics.py
@@ -2,6 +2,11 @@ import string
 import re
 import urllib2
 from bs4 import BeautifulSoup
+import csv
+import os
+
+dirname = os.path.dirname(os.path.abspath(__file__))
+csvfilename = os.path.join(dirname, 'CriticsList.csv')
 
 CRITICS = []
 
@@ -13,22 +18,34 @@ def make_alphabet():
 
 make_alphabet()
 
-reviewer_list_baseurl = "https://www.rottentomatoes.com/critics/authors/?letter=a"
+def get_authors_for_letter(letter):
+    reviewer_list_baseurl = "https://www.rottentomatoes.com/critics/authors/?letter=" + letter
 
-# for letter in alphabet_array:
-#     reviewer_list_one_letter = reviewer_list_baseurl + letter
-#     page = urllib2.urlopen(reviewer_list_one_letter)
-#     soup = BeautifulSoup(page, 'html.parser')
-#     print soup
+    # for letter in alphabet_array:
+    #     reviewer_list_one_letter = reviewer_list_baseurl + letter
+    #     page = urllib2.urlopen(reviewer_list_one_letter)
+    #     soup = BeautifulSoup(page, 'html.parser')
+    #     print soup
 
-page = urllib2.urlopen(reviewer_list_baseurl)
-soup = BeautifulSoup(page, 'html.parser')
+    page = urllib2.urlopen(reviewer_list_baseurl)
+    soup = BeautifulSoup(page, 'html.parser')
 
-for idee in soup.findAll(attrs={'class':'critic-names'}):
-    string = str(idee)
-    isoString = string[42:70]
-    print isoString
-    # print string[var+8]
-    # for i in range(30):
-    #     if i != '\"':
-    #         print string[i]
+    for idee in soup.findAll(attrs={'class':'critic-names'}):
+        criticIso = ""
+        string = str(idee)
+        isoString = string[42:70]
+        for i in isoString:
+            if i != '\"':
+                criticIso += i
+            else:
+                break
+        CRITICS.append(criticIso)
+
+for letter in alphabet_array:
+    get_authors_for_letter(letter)
+
+print CRITICS
+
+resultFile = open(csvfilename, 'w')
+wr = csv.writer(resultFile)
+wr.writerows([CRITICS])
