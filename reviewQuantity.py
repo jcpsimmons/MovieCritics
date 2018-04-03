b4fec52f4c83d8817045e038597fc47c85d151ea
simsies <jcpsimmons@gmail.com>
2018-04-02 19:06:35 -0700

    meh

--- reviewQuantity.py
+++ reviewQuantity.py
@@ -2,19 +2,50 @@ from bs4 import BeautifulSoup
 import sys
 import os
 import csv
+import urllib2
+import re
 
-# open csv
+# critic-name: [score, reviews (-1 is INIT)]
+CRITIC_DICT = {}
+
+def findFirst(search_string, corpus):
+
+    for count, value in enumerate(corpus):
+        begin_clip = 0
+        end_clip = 0
+        if str(value).find(search_string) != -1:
+            #now get just the full number out of this
+            value = str(value)
+            value = value[::-1]
+            for iter, char in enumerate(value):
+                if bool(re.search(r'\d', char)) and end_clip == 0:
+                    end = iter
+            value = value[::-1]
+            for iter, char in enumerate(value):
+                if char == 'f' and begin_clip == 0:
+                    begin_clip = iter + 1
+            print begin_clip
+            print end_clip
+            return value
+
 
 def csvOpen(filename):
-    with open('CriticScores.csv', 'r') as csvfile:
+    with open(filename, 'r') as csvfile:
         csvReader = csv.reader(csvfile)
         for row in csvReader:
-            name_list.append(row)
-    name_list = name_list[0]
-
-
-# make dictionary (multiple entries)
+            CRITIC_DICT[row[0]] = [row[1], -1]
 
 # BS4 loop to scrape number of reviews
+def scrapeNumberOfReviews(critic):
+    test_page = "https://www.rottentomatoes.com/critic/" + critic + "/movies"
+    page = urllib2.urlopen(test_page)
+    soup = BeautifulSoup(page, 'html.parser')
+    soup = soup.select('#criticsReviewsChart_main a')
+    print findFirst('Showing', soup)
+
 
 # save into CSV
+
+# csvOpen('CriticScores.csv')
+
+scrapeNumberOfReviews('armond-white')
